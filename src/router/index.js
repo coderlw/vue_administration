a111a79a161814fdd00956310053c33c607df85d
coderlw <243055343@qq.com>
2021-10-06 17:29:56 +0800

    数据报表完成

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '@/components/goods/Params.vue'
 import GoodsList from '@/components/goods/GoodsList.vue'
 import AddGood from '@/components/goods/AddGood.vue'
 import Order from '@/components/orders/Order.vue'
+import Reports from '@/components/reports/Reports.vue'
 
 Vue.use(Router)
 
@@ -64,6 +65,10 @@ const router = new Router({
     {
       path: '/orders',
       component: Order,
+    },
+    {
+      path: '/reports',
+      component: Reports,
     }
     ]
   }]
